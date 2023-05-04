f8ba2d8a608871bbcf6c9213a8a9160f03f7d8a2
lim ho <dlagh123@gmail.com>
2023-05-04 09:43:13 +0900

    파비콘 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 // import "../style.css";
 
+import Head from 'next/head'
 import { GlobalStyle } from '../components/GlobalStyle'
 import '../style.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <GlobalStyle>
+      <Head>
+        <title>Starbucks</title>
+      </Head>
       <Component {...pageProps} />
     </GlobalStyle>
   )
